6f15068f9f3679faeea8bbdd39448d36257016b1
jgoux <hi@jgoux.dev>
2022-06-14 09:50:47 +0200

    remove generated folder

--- packages/prisma/generated/factories/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { CreateFactoryOptions, CreateFactoryHooks, CreateFactoryReturn } from "prisma-factory";
-import type { ObjectWithMaybeCallbacks } from "prisma-factory";
-import { Prisma, User, Profile, Post, Category } from "/Users/jgoux/Documents/code/acme/node_modules/@prisma/client";
-export declare function createUserFactory(requiredAttrs?: ObjectWithMaybeCallbacks<Partial<Prisma.UserCreateInput>>, options?: CreateFactoryOptions, hooks?: CreateFactoryHooks<Prisma.UserCreateInput, User>): CreateFactoryReturn<Prisma.UserCreateInput, User>;
-export declare function createProfileFactory(requiredAttrs?: ObjectWithMaybeCallbacks<Partial<Prisma.ProfileCreateInput>>, options?: CreateFactoryOptions, hooks?: CreateFactoryHooks<Prisma.ProfileCreateInput, Profile>): CreateFactoryReturn<Prisma.ProfileCreateInput, Profile>;
-export declare function createPostFactory(requiredAttrs?: ObjectWithMaybeCallbacks<Partial<Prisma.PostCreateInput>>, options?: CreateFactoryOptions, hooks?: CreateFactoryHooks<Prisma.PostCreateInput, Post>): CreateFactoryReturn<Prisma.PostCreateInput, Post>;
-export declare function createCategoryFactory(requiredAttrs?: ObjectWithMaybeCallbacks<Partial<Prisma.CategoryCreateInput>>, options?: CreateFactoryOptions, hooks?: CreateFactoryHooks<Prisma.CategoryCreateInput, Category>): CreateFactoryReturn<Prisma.CategoryCreateInput, Category>;
