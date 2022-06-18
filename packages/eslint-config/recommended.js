5f720851a615313e83985cad87ed4481ee4aaa06
jgoux <hi@jgoux.dev>
2022-06-18 17:24:01 +0200

    add an import rule

--- packages/eslint-config/recommended.js
+++ packages/eslint-config/recommended.js
@@ -47,6 +47,7 @@ module.exports = {
     "import/first": "error",
     "import/newline-after-import": "error",
     "import/no-duplicates": "error",
+    "import/no-extraneous-dependencies": "error",
     "simple-import-sort/imports": "error",
     "simple-import-sort/exports": "error",
     "unicorn/prefer-module": "error",
