2b5d5c605bb6a28b7876a1dee4e6a4a55ca77c9c
jgoux <hi@jgoux.dev>
2023-09-15 00:45:00 +0200

    remove dead code

--- apps/api/src/config/drizzle/db.ts
+++ apps/api/src/config/drizzle/db.ts
@@ -7,15 +7,6 @@ const queryClient = postgres(env.DATABASE_URL);
 
 export const db = drizzle(queryClient, { schema });
 
-// const users = await db.query.users.findFirst({
-//   where: (u, _) => _.eq(u.id, 1),
-//   with: {
-//     posts: true,
-//   },
-//   columns: {
-//     fullName: true,
-//   },
-// });
-
 export * from "drizzle-orm";
+
 export * as schema from "./schema.js";
