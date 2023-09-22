d917e92cb080179699f483a7e8ebdeefaa15f4f5
jgoux <hi@jgoux.dev>
2023-09-22 10:35:09 +0200

    remove console

--- apps/cli/src/commands/hello/hello.test.ts
+++ apps/cli/src/commands/hello/hello.test.ts
@@ -3,8 +3,6 @@ import { run } from "#test";
 
 describe("acme hello", () => {
   test("it works", async () => {
-    console.log({ cwd: process.cwd() });
-
     const { stdout } = await run("acme hello Bob");
 
     expect(stdout).toBe("Hello Bob!");
