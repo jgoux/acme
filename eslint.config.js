a3affba98be352a2d7bc5ec3ab446e5e16711d43
jgoux <hi@jgoux.dev>
2023-09-24 22:37:52 +0200

    be more specific in knit config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,9 @@
 import { recommended } from "@acme/eslint-config";
 
-export default recommended;
+export default [
+  {
+    ignores: ["**/{.devenv,.direnv,apps,node_modules,packages}"],
+  },
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  ...recommended,
+];
--- knip.ts
+++ knip.ts
@@ -2,14 +2,11 @@ import { type KnipConfig } from "knip";
 
 const config: KnipConfig = {
   workspaces: {
-    ".": {
-      entry: ["*.(js|ts)"],
-    },
     "apps/*": {
-      entry: ["*.(js|ts)", "src/index.(ts|tsx)"],
+      entry: ["src/index.(ts|tsx)"],
     },
     "packages/*": {
-      entry: ["*.(js|ts)", "src/index.(ts|tsx)"],
+      entry: ["src/index.(ts|tsx)"],
     },
   },
 };
