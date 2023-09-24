2a95716a7a00056cb62580da24061ad26ab7e961
jgoux <hi@jgoux.dev>
2023-09-24 20:02:50 +0200

    yay my changes are in knip!

--- knip.ts
+++ knip.ts
@@ -7,21 +7,9 @@ const config: KnipConfig = {
     },
     "apps/*": {
       entry: ["*.(js|ts)", "src/index.(ts|tsx)"],
-      playwright: {
-        entry: ["playwright.config.{js,ts}", "playwright/index.tsx"],
-      },
-      storybook: {
-        config: [".storybook/{main,manager,test-runner}.{js,ts}"],
-      },
-      typescript: {
-        config: ["tsconfig.json", "tsconfig.*.json"],
-      },
     },
     "packages/*": {
       entry: ["*.(js|ts)", "src/index.(ts|tsx)"],
-      typescript: {
-        config: ["tsconfig.json", "tsconfig.*.json"],
-      },
     },
   },
 };
