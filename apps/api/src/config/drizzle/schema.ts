e7703a8454137e5ff62f064444a16b8c2dfb6045
jgoux <hi@jgoux.dev>
2023-09-24 21:54:30 +0200

    mark drizzle exports as public

--- apps/api/src/config/drizzle/schema.ts
+++ apps/api/src/config/drizzle/schema.ts
@@ -1,21 +1,33 @@
 import { relations } from "drizzle-orm";
 import * as d from "drizzle-orm/pg-core";
 
+/**
+ * @public
+ */
 export const users = d.pgTable("users", {
   id: d.serial("id").primaryKey(),
   fullName: d.text("full_name"),
 });
 
+/**
+ * @public
+ */
 export const usersRelations = relations(users, ({ many }) => ({
   posts: many(posts),
 }));
 
+/**
+ * @public
+ */
 export const posts = d.pgTable("posts", {
   id: d.serial("id").primaryKey(),
   content: d.text("content"),
   writtenBy: d.integer("written_by"),
 });
 
+/**
+ * @public
+ */
 export const postsRelations = relations(posts, ({ one }) => ({
   author: one(users, {
     fields: [posts.writtenBy],
