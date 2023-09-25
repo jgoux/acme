e548a2d66b9f39a46f5483e2ab01e956c4498f8c
jgoux <hi@jgoux.dev>
2023-09-25 19:34:35 +0200

    mutualize global types

--- apps/web/global.d.ts
+++ apps/web/global.d.ts
@@ -1,1 +1,2 @@
 import "@total-typescript/ts-reset";
+import "vite/client";
--- knip.ts
+++ knip.ts
@@ -3,10 +3,10 @@ import { type KnipConfig } from "knip";
 const config: KnipConfig = {
   workspaces: {
     "apps/*": {
-      entry: ["src/index.(ts|tsx)"],
+      entry: ["src/index.(ts|tsx)", "global.d.ts"],
     },
     "packages/*": {
-      entry: ["src/index.(ts|tsx)"],
+      entry: ["src/index.(ts|tsx)", "global.d.ts"],
     },
   },
 };
