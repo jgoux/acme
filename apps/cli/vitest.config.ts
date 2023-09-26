e727c95c37abe3ba54fa49bb08e95ee6faa7862e
jgoux <hi@jgoux.dev>
2023-09-26 22:28:11 +0200

    fix path for knip

--- apps/cli/vitest.config.ts
+++ apps/cli/vitest.config.ts
@@ -2,6 +2,6 @@ import { defineProject } from "vitest/config";
 
 export default defineProject({
   test: {
-    globalSetup: "test/global-setup.ts",
+    globalSetup: "./test/global-setup.ts",
   },
 });
