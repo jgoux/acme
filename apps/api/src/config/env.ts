d90c2dbf0763ef963fcda6365e6831e0f42e52ff
jgoux <hi@jgoux.dev>
2023-10-08 22:04:17 +0200

    satisfy env for now

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -2,7 +2,10 @@ import * as v from "valibot";
 
 export const env = v.parse(
   v.object({
-    DATABASE_URL: v.string(),
+    DATABASE_URL: v.optional(
+      v.string(),
+      "postgresql://postgres@localhost:2345/acme",
+    ),
   }),
   process.env,
 );
