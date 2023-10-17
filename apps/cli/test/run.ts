adb87d9f9991963bdb529b9e012d0b5b1bd686ad
jgoux <hi@jgoux.dev>
2023-10-17 08:21:27 +0200

    trying bun

--- apps/cli/test/run.ts
+++ apps/cli/test/run.ts
@@ -4,9 +4,7 @@ import { fileURLToPath } from "node:url";
 export async function run(command: string, options?: Options) {
   const processedCommand = command.replace(
     "acme",
-    process.env["CI"]
-      ? "node dist/index.js"
-      : "tsx --conditions development src/index.ts",
+    process.env["CI"] ? "node dist/index.js" : "bun src/index.ts",
   );
 
   return execaCommand(processedCommand, {
