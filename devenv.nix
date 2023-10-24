0d086cf8b32f81b43ea405e6fcabcc057cb413b5
jgoux <hi@jgoux.dev>
2023-10-24 09:15:56 +0200

    Revert "try flake"
    This reverts commit 5fe9600d92a16fb524fd1ae9511b5d9b09932cda.

---
+++ devenv.nix
@@ -0,0 +1,46 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.PLAYWRIGHT_BROWSERS_PATH="${pkgs.playwright-driver.browsers}";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.infisical
+    pkgs.git
+    pkgs.playwright-driver
+  ];
+
+  # https://devenv.sh/services/
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql_16;
+    initialDatabases = [{ name = "acme"; }];
+    listen_addresses = "localhost";
+    port = 2345;
+    initialScript = ''
+      CREATE USER postgres SUPERUSER;
+    '';
+  };
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = "echo hello from $GREET";
+
+  # enterShell = ''
+  #   hello
+  #   git --version
+  # '';
+
+  # https://devenv.sh/languages/
+  languages.javascript.enable = true;
+  languages.javascript.package = pkgs.nodejs_18;
+  languages.javascript.corepack.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
