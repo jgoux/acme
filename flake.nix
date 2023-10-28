8b7fb3ca172a97d23a4efe65c4ecd2fde80ad088
jgoux <hi@jgoux.dev>
2023-10-28 22:18:08 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+
+    devenv.url = "github:cachix/devenv/python-rewrite";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://cache.nixos.org https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, devenv } @ inputs:
+
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [ (import ./devenv.nix) ];
+        };
+      });
+
+}
