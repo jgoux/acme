1c2afb18c295761adf421d3a0353064daa91e5a8
jgoux <hi@jgoux.dev>
2023-10-29 10:19:45 +0100

    remove substituter

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
   };
 
   nixConfig = {
-    extra-trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://cache.nixos.org https://devenv.cachix.org";
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
   };
 
   outputs = { self, nixpkgs, flake-utils, devenv } @ inputs:
